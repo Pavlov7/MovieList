fdcbfe74b545be1d042215b581277b642b2654f8
Pavlov7 <ieetcatspr@gmail.com>
2017-04-22 20:42:08 +0300

    created project - initial commit

---
+++ src/main/java/pavlov/movie_list/MovieListApplication.java
@@ -0,0 +1,12 @@
+package pavlov.movie_list;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieListApplication.class, args);
+	}
+}
---
+++ src/main/java/pavlov/movie_list/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package pavlov.movie_list.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by Daniel on 22-Apr-17.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  extends WebSecurityConfigurerAdapter{
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/front-end/**").permitAll()
+                //.antMatchers("/users").hasRole("ADMIN")
+                .anyRequest().authenticated()
+                .and().csrf().disable();
+    }
+}
